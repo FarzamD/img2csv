2041d91fe9a9a6fb7626b6ab4f6d73b40830df43
Farzam <takfarzam@gmail.com>
2023-07-05 20:37:01 +0330

    ignore certificate error

--- app/main.js
+++ app/main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 let mainWidnow = null;
+// ignore certificate error
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(chrome_options=options)
+
 app.on('ready', () => {
 	console.log('Hello from Electron');
 	mainWidnow = new BrowserWindow();
