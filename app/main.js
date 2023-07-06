eb9698aeeccc8105149ef5a467f91d7e90f1f934
Farzam <takfarzam@gmail.com>
2023-07-06 05:16:15 +0330

    adding more style

--- app/main.js
+++ app/main.js
@@ -1,10 +1,46 @@
 const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+
 let mainWidnow = null;
 // ignore certificate error
-app.commandLine.appendSwitch('ignore-certificate-errors');
 
 app.on('ready', () => {
+	app.commandLine.appendSwitch('ignore-certificate-errors');
+
 	console.log('Hello from Electron');
-	mainWidnow = new BrowserWindow();
-	mainWidnow.webContents.loadURL(`file://${__dirname}/index.html`);
+	mainWidnow = new BrowserWindow({
+				height: 800,
+				width: 600,
+				show: false,
+				frame: false,
+				backgroundColor: '#8c8a7a;'});
+
+	mainWidnow.webContents.loadURL(
+					url.format({
+						pathname: path.join(__dirname, 'index.html'),
+						protocol: 'file:',
+						slashes: true ///
+					})
+				);
+			
+	mainWidnow.setBackgroundColor('#8c8a7a');
+
+	mainWidnow.once('ready-to-show', () => {
+		mainWidnow.show();
+	});
+	mainWidnow.on('closed', () => {
+		mainWidnow = null;
+	});
+	
+
+
+
+
+
+
+
+
+
 });
+
