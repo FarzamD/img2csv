169ef0a8e8b58013e5336a09abf2760e58f562f8
Farzam <takfarzam@gmail.com>
2023-12-15 17:55:56 +0330

    tkinter app

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ consists of three projects:
 3. image to csv electron app
 
 + python image to csv:
-    + takes an image
     + detects table lines
     + makes a table
     + extracts texts from image to cells
@@ -18,7 +17,6 @@ consists of three projects:
 + image to csv electron app:
     + an app made with electron
     + added GUI to python app and contains some python code
-
 
  *** 
 ## python image to csv
@@ -51,7 +49,28 @@ pip install pytesseract
 
  ***
 ## python image to csv with tkinter UI
-
+### pre-requisites
++ install opencv
+    ```linux
+    pip install cv2
+    ```
+    or
+    ```linux
+    apt-get install python-OpenCV
+    ```
++ install Tesseract
++ install pytesseract
+    ```linux
+    pip install pytesseract
+    ```
+### code blocks
++ processing codes are the same as previous project
++ only UI code was added in this project
+#### UI
++ UI code consists of two tabs:
+    + image tab: where uploaded image and detected lines are displayed
+    + table tab: where detected table is displayed and lets you choose as a file 
++ ***image tab:***
 
  ***
 ## image to csv electron app
@@ -59,5 +78,4 @@ pip install pytesseract
 ```python
 import pytesseract
 text = pytesseract.image_to_string(img)
-
 ```
--- tkinter-app/img2csv.py
+++ tkinter-app/img2csv.py
@@ -23,8 +23,8 @@ def Edge(image):
 def line_threshold(shape, alternative=140):
     trh= min(shape)*2//5
     return max(trh, alternative)
-def line_detection(image):
-    return cv2.HoughLines(image, 1, np.pi/180, line_threshold(image.shape))
+def line_detection(image,alternative=140):
+    return cv2.HoughLines(image, 1, np.pi/180, line_threshold(image.shape,alternative=alternative))
 def plot_lines(lines, image):#array version of plot_lines
     img = image.copy()
     lines_r= lines.reshape((-1,2)).astype(np.float64)
@@ -109,6 +109,7 @@ class ImageUploaderApp:
     def __init__(self, master):
         self.master = master
         self.master.title("table extractor")
+        self.alt=140
 
         #tab control
         self.tabControl = ttk.Notebook(master)
@@ -130,34 +131,54 @@ class ImageUploaderApp:
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
 
         if file_path:
-            image = cv2.imread(file_path)
-            img = cv2tk(image)
+            self.image = cv2.imread(file_path)
+            img = cv2tk(self.image)
             self.image_label.config(image=img)
             self.image_label.image = img
 
-            edges = Edge(image)
-            lines= line_detection(edges)
+            self.edges = Edge(self.image)
 
-            lines_plt= plot_lines(lines, image)            
-            imgl= cv2tk(lines_plt)
             tk.Label(self.image_frame, text="detected lines:").pack()
+
+            self.lines= line_detection(self.edges)
+            lines_plt= plot_lines(self.lines, self.image)            
+            self.imgl= cv2tk(lines_plt)
+
             self.image_lines_label = tk.Label(self.image_frame)
             self.image_lines_label.pack()
-            self.image_lines_label.config(image=imgl)
-            self.image_lines_label.image = imgl
-
-            p=corners(lines, image)
-            csv= table(image, p)
-            self.df= pd.DataFrame(csv)
+            self.show_lines()
             
+            button_frame = tk.Frame(self.image_frame)
+            button_frame.pack()
+
+            incp_button= tk.Button(button_frame, text="detect way more lines", command=self.morep_lines)
+            incp_button.pack(pady=20, padx=20, side=tk.LEFT)
+            inc_button= tk.Button(button_frame, text="detect more lines", command=self.more_lines)
+            # inc_button.pack(row=4, column=0, pady=20, padx=20)
+            inc_button.pack(pady=20, padx=20, side=tk.LEFT)
+            dec_button= tk.Button(button_frame, text="detect less lines", command=self.less_lines)
+            # dec_button.pack(row=4, column=1, pady=20, padx=20)
+            dec_button.pack(pady=20, padx=20, side=tk.LEFT)
+            dec_button= tk.Button(self.image_frame, text="confirm lines", command=self.conf_lines)
+            # dec_button.pack(row=4, column=1, pady=20, padx=20)
+            dec_button.pack(pady=20, padx=20,side=tk.TOP)
+
+            self.create_df()
             self.create_table_tab()
     def create_table_tab(self):
         #table
-        tk.Label(self.table_frame, text="detected table:").pack()
+        self.det_tables_lbl= tk.Label(self.table_frame, text="detected table:")
+        self.det_tables_lbl.pack()
         self.unpack_table()
         # confirm table and save
-        save_button= tk.Button(self.table_frame, text="Confirm & Save table", command=self.save_table)
-        save_button.pack(pady=20)
+        self.save_button= tk.Button(self.table_frame, text="Confirm & Save table", command=self.save_table)
+        self.save_button.pack(pady=20)
+
+    def create_df(self):
+        p=corners(self.lines, self.image)
+        csv= table(self.image, p)
+        self.df= pd.DataFrame(csv)
+        
 
     def unpack_table(self):
         self.tree = ttk.Treeview(self.table_frame, columns=tuple(self.df.columns))
@@ -170,6 +191,36 @@ class ImageUploaderApp:
         file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
         if file_path:
             self.df.to_csv(file_path)
+    def show_lines(self):
+        self.image_lines_label.config(image=self.imgl)
+        self.image_lines_label.image = self.imgl
+    def less_lines(self):
+        self.alt+=2
+        self.lines= line_detection(self.edges, alternative=self.alt)
+        lines_plt= plot_lines(self.lines, self.image)            
+        self.imgl= cv2tk(lines_plt)
+        self.show_lines()
+    def more_lines(self):
+        self.alt-=3
+        self.lines= line_detection(self.edges, alternative=self.alt)
+        lines_plt= plot_lines(self.lines, self.image)            
+        self.imgl= cv2tk(lines_plt)
+        self.show_lines()
+    def morep_lines(self):
+        self.alt-=10
+        self.lines= line_detection(self.edges, alternative=self.alt)
+        lines_plt= plot_lines(self.lines, self.image)            
+        self.imgl= cv2tk(lines_plt)
+        self.show_lines()
+    def conf_lines(self):
+        self.create_df()
+        
+        self.det_tables_lbl.destroy()
+        self.save_button.destroy()
+        self.tree.destroy()
+
+        self.create_table_tab()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
